77f3d12e2cdf3a88482e4643491e763f4bbf3caa
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-09-23 19:17:35 -0400

    styled and move to center the add to cart button

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,6 +7,10 @@ for (let i=0; i < data.length; ++i) {
     let newDiv = document.createElement('div');
     newDiv.className = 'item'
 
+    // create a new div element and give it a class name for the buttons
+    let bottonDiv = document.createElement('div');
+    bottonDiv.className = 'botton-div'
+
     // create an image element
     let img = document.createElement('img');
     // this will change each time we go through the loop. Can you explain why?
@@ -16,7 +20,6 @@ for (let i=0; i < data.length; ++i) {
 
     // Add the image to the div
     newDiv.appendChild(img)
-
 
     // create a paragraph element for the descrpition
     let desc = document.createElement('p');
@@ -38,10 +41,13 @@ for (let i=0; i < data.length; ++i) {
     button.id = data[i].name
 
     // creates a custom attribute called data-price
-    // that will hold the prie for each element in the button
+    // that will hold the price for each element in the button
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
+    bottonDiv.appendChild(button)
+
+    // add the botton element its own div
+    newDiv.appendChild(bottonDiv)
 
     // put new div inside items container
     itemsContainer.appendChild(newDiv)
